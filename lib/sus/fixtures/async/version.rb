c1ba02b7ae47684266be112b2fc541db84f54138
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2023-08-27 17:10:55 +1200

    Modernize gem.

--- lib/sus/fixtures/async/version.rb
+++ lib/sus/fixtures/async/version.rb
@@ -1,8 +1,7 @@
-# Copyright, 2019, by Samuel G. D. Williams. <http: //www.codeotaku.com>
 # frozen_string_literal: true
 
 # Released under the MIT License.
-# Copyright, 2022, by Samuel Williams.
+# Copyright, 2022-2023, by Samuel Williams.
 
 module Sus
 	module Fixtures
