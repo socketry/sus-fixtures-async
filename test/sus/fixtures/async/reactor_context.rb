72a0fae401f60aa907baa1f5dd602a2ea937c653
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2024-07-28 10:25:46 +1200

    Add more tests.

--- test/sus/fixtures/async/reactor_context.rb
+++ test/sus/fixtures/async/reactor_context.rb
@@ -5,6 +5,28 @@
 
 require 'sus/fixtures/async/reactor_context'
 
+module NestedContext
+	def before
+		super
+		
+		expect(reactor).to be_a(Async::Reactor)
+	end
+	
+	def after
+		expect(reactor).to be_a(Async::Reactor)
+		
+		super
+	end
+	
+	def around(&block)
+		super do
+			expect(reactor).to be_a(Async::Reactor)
+			
+			block.call
+		end
+	end
+end
+
 describe Sus::Fixtures::Async::ReactorContext do
 	with 'reactor context' do
 		include Sus::Fixtures::Async::ReactorContext
@@ -24,6 +46,14 @@ describe Sus::Fixtures::Async::ReactorContext do
 					task.sleep(0.1)
 				end
 			end.to raise_exception(Async::TimeoutError)
+		end
+		
+		with 'nested context' do
+			include NestedContext
+			
+			it "runs in a reactor" do
+				expect(reactor).to be_a(Async::Reactor)
+			end
 		end
 	end
 end
